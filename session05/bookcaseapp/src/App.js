c07ac1c4c60b8a53970bfde7f1f83a5be53cc893
shadies <adewodufolashade@gmail.com>
2022-03-23 16:29:04 +0100

    handling events

--- session05/bookcaseapp/src/App.js
+++ session05/bookcaseapp/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import Book from './components/Book';
-import bookList from './models/books.json';
+import React, {useState} from 'react';
+import BookList from './components/BookList';
+import bookData from './models/books.json';
 
 function App() {
-  const books = bookList;
-  return <Book/>;
+  const [books]= useState (bookData);
+  
+  return <BookList books = {books} addBook ={addBook}> </BookList>;
+}
+
+function addBook(title) {
+  console.log(`The Book ${title} was clicked`);
 }
 
 export default App;
